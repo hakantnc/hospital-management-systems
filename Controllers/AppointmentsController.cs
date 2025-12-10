2bcc4574ae0cbdf81641eb7a563d4e6ecd6c2c0a
hakantnc <tnchakan123@gmail.com>
2025-12-11 01:20:56 +0300

    add: Doctor Panel, Appointment System and refactor some authentication system

---
+++ Controllers/AppointmentsController.cs
@@ -0,0 +1,91 @@
+﻿using HospitalSystem.Data;
+using HospitalSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HospitalSystem.Controllers
+{
+    public class AppointmentsController : Controller
+    {
+        HospitalContext db = new HospitalContext();
+        // GET: Appointments
+        public ActionResult Index()
+        {
+            if (Session["UserID"] == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            int currentUserId = (int)Session["UserID"];
+
+            var appointments = db.Appointments
+                                 .Where(a => a.PatientID == currentUserId)
+                                 .Include(a => a.Doctor)
+                                 .Include(a => a.Doctor.User)
+                                 .Include(a => a.Doctor.Department)
+                                 .OrderByDescending(a => a.AppointmentDateTime) //en yenisini en üste getirmek için
+                                 .ToList();
+
+            return View(appointments);
+        }
+
+        public ActionResult Create()
+            {
+            if (Session["UserID"] == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            ViewBag.DepartmentID = new SelectList(db.Departments, "DepartmentID", "DepartmentName");
+            return View();
+        }
+
+        public JsonResult GetDoctorByDepartment(int departmentId) { 
+        
+            var doctors = db.Doctors
+                            .Where(d => d.DepartmentID == departmentId)
+                            .Include("User")
+                            .Select(d => new
+                            {
+                                Value = d.UserID,
+                                Text = d.User.FirstName + " " + d.User.LastName
+                            })
+                            .ToList();
+            return Json(doctors, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Appointment appointment)
+        {
+            if (Session["UserID"] == null) return RedirectToAction("Login", "Account");
+            appointment.PatientID = (int)Session["UserID"];
+            appointment.Status = "Aktif";
+
+            if(appointment.AppointmentDateTime < DateTime.Now)
+            {
+                ModelState.AddModelError("", "Geçmiş Bir Tarihe Randevu Alamazsınız.");
+            }
+            bool isBusy = db.Appointments.Any(
+                a => a.DoctorID == appointment.DoctorID
+                &&
+                a.AppointmentDateTime == appointment.AppointmentDateTime && a.Status != "İptal");
+        
+            if(isBusy)
+            {
+                ModelState.AddModelError("", "Seçtiğiniz Saatte Doktorun Başka Randevusu Var.");
+            }
+            if(ModelState.IsValid)
+            {
+                db.Appointments.Add(appointment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DepartmentID = new SelectList(db.Departments, "DepartmentID", "DepartmentName");
+            return View(appointment);
+
+        }
+    }
+}
---
+++ Controllers/DoctorController.cs
@@ -0,0 +1,48 @@
+﻿using HospitalSystem.Data;
+using HospitalSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HospitalSystem.Controllers
+{
+    public class DoctorController : Controller
+    {
+        HospitalContext db = new HospitalContext();
+        // GET: Doctor
+        public ActionResult Index()
+        {
+            if (Session["UserID"] == null || Session["UserRole"].ToString() != "Doctor")
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            int currentUserId = (int)Session["UserID"];
+            var doctor = db.Doctors.FirstOrDefault(d => d.UserID == currentUserId);
+
+            if (doctor == null)
+            {
+                ViewBag.Error = "Doktor profiliniz bulunamadı.";
+                return View(new System.Collections.Generic.List<Appointment>());
+            }
+            var today = DateTime.Today;
+            ViewBag.TodayCount = db.Appointments
+                                   .Count(a => a.DoctorID == doctor.UserID &&
+                                               DbFunctions.TruncateTime(a.AppointmentDateTime) == today &&
+                                               a.Status == "Aktif");
+
+            var appointments = db.Appointments
+                                 .Where(a => a.DoctorID == doctor.UserID)
+                                 .Include(a => a.Patient)
+                                 .Include(a => a.Patient.User)
+                                 .OrderByDescending(a => a.AppointmentDateTime)
+                                 .ToList();
+
+            return View(appointments);
+
+            return View();
+        }
+    }
+}
